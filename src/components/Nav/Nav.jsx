5b01c0374485f3e8ef20dbfc7a9343d78ec5c750
Christian Carlson <christiancarlson648@gmail.com>
2024-01-02 14:40:18 -0600

    Added a bunch of CSS elements

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -16,9 +16,15 @@ function Nav() {
         {/* If no user is logged in, show these links */}
         {!user.id && (
           // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
+          <>
+            <Link className="navLink" to="/login">
+              Login / Register
+            </Link>
+            <Link className="navLink" to="/about">
+            About
+            </Link>
+          </>
+          
         )}
 
         {/* If a user is logged in, show these links */}
@@ -32,13 +38,14 @@ function Nav() {
               Info Page
             </Link>
 
+            <Link className="navLink" to="/about">
+              About
+            </Link>
+
             <LogOutButton className="navLink" />
           </>
         )}
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
       </div>
     </div>
   );
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import "./UserPage.css"
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
+    <>
+    <section>
+      <div className="container">
+        <h2>Welcome, {user.username}</h2>
+        {/* <p>Your ID is: {user.id}</p> */}
+      </div>
+      <div id="background-wrap">
+        <div class="x1">
+          <div class="cloud"></div>
+        </div>
+        <div class="x2">
+          <div class="cloud"></div>
+        </div>
+        <div class="x3">
+          <div class="cloud"></div>
+        </div>
+        <div class="x4">
+          <div class="cloud"></div>
+        </div>
+      </div>
+    </section>
+
+    <section class="water">
+      <div class="wave wave1"></div>
+      <div class="wave wave2"></div>
+      <div class="wave wave3"></div>
+      <div class="wave wave4"></div>
+    </section>
+
+    <section class="info">
+      
+    </section>
+    </>
   );
 }
 
